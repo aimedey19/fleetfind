f829f572d5b5b4df505b55a01d752f8e6a03b5b0
da-kodesio <da.kodesio@gmail.com>
2024-01-23 11:16:09 +0100

    Models registration update

---
+++ fleetfind/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class WatsonAppConfig(AppConfig):
+    """App configuration for watson."""
+    name = 'fleetfind'
+    default_auto_field = 'django.db.models.AutoField'
