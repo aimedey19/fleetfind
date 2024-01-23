31b4ef1af0577e5b9fbe33ae11252c85e7d626da
da-kodesio <da.kodesio@gmail.com>
2024-01-23 20:42:00 +0100

    Building for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fleetfind',
-    version='1.0.1',
+    version='1.1.0',
     packages=find_packages(),
     install_requires=[
         # List your dependencies here
